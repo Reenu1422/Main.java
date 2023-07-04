119575e61afa1be9cadff2f5bed70be5be5d5d38
reenu1422 <reenuslnk@gmail.com>
2023-07-04 12:54:49 +0530

    hi

---
+++ Main.java
@@ -0,0 +1,32 @@
+import javax.swing.plaf.synth.SynthGraphicsUtils;
+import java.net.StandardSocketOptions;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }}
